8aebd03d28a40e83828355e54790b478614da79b
İsmail Can VARLI <icanvarli@outlook.com>
2023-02-23 23:20:02 +0300

    Table handling kısmını işledim

---
+++ TableHandling.cy.js
@@ -0,0 +1,36 @@
+/// <reference types = "Cypress" />
+
+/*
+Bu kısımda bir tabloda ki satır ve sütun sayısını öğreneceğiz
+İstediğimiz satır ve sütundaki veriye erişmeyi öğreneceğiz
+Tüm satır ve sütunlardaki verileri okumayı göreceğiz
+Pagination(sayfalandırmayı) göreceğiz
+*/
+
+describe("Table Handling ", ()=>{
+
+//Bu kısımda bir tabloda ki satır ve sütun sayısını öğreneceğiz
+    it("Check number rows & columns",()=>{
+    
+    
+    })
+
+//İstediğimiz satır ve sütundaki veriye erişmeyi öğreneceğiz    
+    it("Check cell data from specific rows & column",()=>{
+    
+    
+    })
+
+//Tüm satır ve sütunlardaki verileri okumayı göreceğiz    
+    it("Read all the rows & columns data in the first page",()=>{
+    
+    
+    })
+
+//Pagination(sayfalandırmayı) göreceğiz    
+    it("Pagination (Sayfalandırma)",()=>{
+    
+    
+    })
+    
+})
